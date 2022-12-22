de662f3cf2f4d06dbe44a0432dd2619f4eadc8b0
barrosdvlpr <barrosdvlpr@gmail.com>
2022-12-22 04:10:31 -0300

    (introduction to static typing

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,3 +6,15 @@ export default function Home() {
     </div>
   )
 }
+
+let someString: string = "cool";
+
+// someString = 243233;
+
+let coolFunc = (a: string, b: number) => a + b;
+
+coolFunc(
+  someString, 
+  1212,
+  // {}
+  )
