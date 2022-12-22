98d03290c8038933b65f6b5ccbbd6d54ef8efda1
barrosdvlpr <barrosdvlpr@gmail.com>
2022-12-22 05:15:03 -0300

    Using Interfaces to Describe Types

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,86 @@ export default function Home() {
   )
 }
 
-let unit: string = "awesome";
-// let miles: "MILES" = "MILES"; 
-let miles: "MILES" = "MILtS"; 
+// let superHero: { secretIdentity: string; superHeroName: string; health: number };
+// let superVillain: { secretIdentity: string; superHeroName: string; health: number };
 
-// function moveCharacter(distance: number, value: string) {
-//   console.log(`You moved ${distance} ${value}`);
+// let superHero: { secretIdentity: string; alias: string; health: number };
+// let superVillain: { secretIdentity: string; alias: string; health: number };
+
+interface AttackFunction {
+  (opponent: { alias: string; health: number;}, attackWith: number): number;
+}
+
+interface KrustyTheClown extends OptionalAttributes {
+  secretIdentity?: string;
+  alias: string;
+  health: number;
+  attack: AttackFunction;
+}
+interface ComicBookCharacter extends OptionalAttributes {
+  secretIdentity?: string;
+  alias: string;
+  health: number;
+  // attack(opponent: { alias: string; health: number;} attackWith: number): number;
+  attack: AttackFunction;
+}
+
+interface OptionalAttributes {
+  strength?: number;
+  insanity?: number;
+  dexterity?: number;
+  healingFactor?: number;
+}
+
+// let superHero: ComicBookCharacter = {
+//   alias: true,
+//   health: 5000
+// }
+
+// let superHero = {
+//   alias: true,
+//   health: 5000
 // };
-// // moveCharacter(3, "feet");
-// moveCharacter(4, "dragon");
 
-type distanceMetric = "MILES" | "KILOMETERS" | "METERS" | "YARDS" | "FEET" | "INCHES";
-function moveCharacter(distance: number, value: distanceMetric) {
-  console.log(`You moved ${distance} ${value}`);
-};
-// moveCharacter(4, "dragon");
+// let superHero: ComicBookCharacter = {
+//   alias: "Hulk",
+//   health: 5000
+// }
 
+// let superVillain: ComicBookCharacter = {
+//   secretIdentity: "Jack Napier",
+//   alias: "Joker",
+//   health: 75
+// }
+
+function attackFunc(opponent, attackWith) {
+  opponent.health -= attackWith;
+  console.log(`${this.alias} attacked ${opponent.alias}, who's health = ${opponent.health}`);
+  return opponent.health;
+}
+let superHero: ComicBookCharacter = {
+  // alias: true, 
+  alias: "Hulk",      
+  health: 5000,
+  strength: 5000,
+  attack: attackFunc      
+}
+let superVillain: ComicBookCharacter = {
+  scretIdentity: "Jack Napier", 
+  alias: "Joker",               
+  health: 75,
+  insanity: 175,
+  attack: attackFunc
+}
+
+superHero.attack(superVillain, superHero.strength);
+
+
+function getSecretIdentity(character: ComicBookCharacter) {
+  if (character.secretIdentity) {
+    console.log(`${character.alias} is ${character.secretIdentity}`);
+  } else {
+    console.log(`${character.alias} has no secret identity`);
+  }
+}
+getSecretIdentity(superHero); 
