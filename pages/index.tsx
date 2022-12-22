f02869f053d589e096d4d518b4d0b98b7e0c89bd
barrosdvlpr <barrosdvlpr@gmail.com>
2022-12-22 04:40:17 -0300

    Union Types and Type Aliases in TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,58 @@ export default function Home() {
   )
 }
 
-let userName: string = "Silver Surfer";
-// userName = ["Silver", "Surfer"];
+// const thing: string | number;
 
-// let userId = (a: string, b:number): string => a + b;
-// let userId = (a: string, b:number) => a + b;
-// let userId = (a: string, b:number): number => a + b;
-let userId = (a: string, b:number) => a + b;
+// const thing: string | number | string[] | boolean;
+// const returnSomething = (someThing: string | number | string[] | boolean) => {
+//   return someThing;
+// };
 
-// let target: HTMLElement = document.getElementById("target");
-// target.onclick = (event: MouseEvent) => event.button;
+// type thing = string | number | string[] | boolean;
+// const returnSomeThing = (someThing: thing) => someThing;
+// console.log(returnSomeThing("coolGuy"));
 
-let target: HTMLElement = document.getElementById("target");
-target.onclick = (event: HTMLButtonElement) => event.button;  
+// type thing = string | number | string[] | boolean;
+// const returnSomeThing = (someThing: thing) => {
+//   if (typeof someThing === "string" || typeof someThing === "number" || typeof someThing === "boolean") {
+//     console.log("someThing", someThing);
+//   }
+//   if (someThing instanceof Array) {
+//     const joinedThings = "";
+//     someThing.forEach(thing => {
+//       joinedThings += `${thing}`;
+//     })
+//     console.log("joinedThings", joinedThings);
+//   }
+// }
+// // returnSomeThing(2323)
+// returnSomeThing(["Wonder", "Woman", "Rocks!!!!"])
+
+type thing = string | number | string[] | boolean;
+const returnSomeThing = (someThing: thing) => {
+  if (someThing instanceof Array) {
+    const joinedThings = "";
+    someThing.forEach(thing => {
+      joinedThings += `${thing}`;
+    })
+    console.log("joinedThings", joinedThings);
+  } else {console.log("something =", someThing);}
+}
+returnSomeThing(false)
+
+type stuff = string | {name:string};
+const gimmeStuff = (stuff: stuff) => {
+  typeof stuff === "string";
+  typeof stuff.name === "string";   
+};
+
+type coolThings = {name: string;} | {id: number;};
+const gimmeCoolthings = (thing: coolThings) => {
+  if (typeof thing.name === "string") { return thing.name; }
+  if (typeof thing.id === "number") { return thing.id; }
+};  
+
+type stuffAndThings = {cool: string; meh: string;} | {cool: string; lame: string; }
+const gimmeStuffAndThings = (sat: stuffandThings) => {
+  return sat.cool || sat.lame;
+};
